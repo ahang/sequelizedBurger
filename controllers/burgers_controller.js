a981a01141244bff5cfc08c267d720cf86120acb
ahang <hang.andy.t@gmail.com>
2017-05-04 10:20:06 -0500

    updated pre-sequelize

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,40 +4,19 @@ var express = require("express");
 var router = express.Router();
 
 //Importing the model to use database function
-var burger = require("../models/burger.js");
+var db = require("../models/burger.js");
 
 //Creating routes and logic for each route
 router.get("/", function(req, res) {
-    //grabbing SQL String & MySQL DB connection and returning a data to hand off to handlebar template
-    burger.all(function(data) {
-        var hbsObject = {
-            burgers: data
-        };
-        console.log(hbsObject);
-        res.render("index", hbsObject);
-    });
+
 });
 
 router.post("/", function(req, res) {
-    //console.log(req.params.burger_name);
-    //grabbing burger name to add to the MySQL DB
-    console.log(req.body.burger);
-    burger.newBurger([
-        req.body.burger
-    ], function() {
-        res.redirect("/");
-    });
+
 });
 
 router.put("/:id", function(req, res) {
-    //Updating our DB with the appropriate ID and value from the request
-    console.log(req.body);
-    console.log("The ID " + req.params.id);
-    console.log("Value of Devoured " + req.body.devoured);
-    burger.eaten(
-        req.body.devoured, req.params.id, function() {
-            res.redirect("/");
-    });
+
 });
 
 //Export routes for server.js
--- models/burger.js
+++ models/burger.js
@@ -1,28 +1,17 @@
 //Importing the ORM to create functions to interact with the database
-var orm = require("../config/orm.js");
 console.log("Burger Models Operational");
 
 var burger = {
     all: function(callback) {
-        //Querying all items from the burgesr table
-        orm.selectAll("burgers", function(result) {
-            callback(result);
-        });
+
     },
 
     newBurger: function(vals, callback) {
-        //Inserting a burger into the burgers table
-        orm.insertOne("burgers", "burger_name", vals, function(result) {
-            //console.log("Trying to callback function in models");
-            callback(result);
-        });
+
     },
 
     eaten: function(newInput, pageId, callback) {
-        //Updating devoured column to true based on burger ID
-        orm.updateOne("burgers", "devoured", newInput, "id", pageId, function(result) {
-            callback(result);
-        });
+
     }
 };
 
